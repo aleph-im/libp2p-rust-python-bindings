a6bb39c539b6e0242408ac201f0a1d6bb478b5b5
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-05-03 15:49:20 +0200

    example

--- examples/identity.py
+++ examples/identity.py
@@ -2,7 +2,8 @@ import libp2p_rust_python_bindings as libp2p
 
 
 def main():
-    keypair = libp2p.identity.PyKeypair.generate_ed25519()
+    keypair = libp2p.identity.Keypair.generate_ed25519()
+    print(keypair)
     public_key = keypair.public()
     print(public_key)
 
--- src/core/identity.rs
+++ src/core/identity.rs
@@ -1,13 +1,14 @@
 use libp2p::core::PublicKey;
 use pyo3::prelude::*;
 use libp2p::identity;
+use pyo3::py_run;
 
-#[pyclass]
+#[pyclass(name="Keypair")]
 struct PyKeypair {
     keypair: identity::Keypair,
 }
 
-#[pyclass]
+#[pyclass(name="PublicKey")]
 struct PyPublicKey {
     public_key: PublicKey,
 }
@@ -34,6 +35,9 @@ pub(crate) fn register(py: Python<'_>, m: &PyModule) -> PyResult<()> {
     let identity_module = PyModule::new(py, "identity")?;
     identity_module.add_class::<PyKeypair>()?;
     identity_module.add_class::<PyPublicKey>()?;
+
+    // See https://github.com/PyO3/pyo3/issues/1517#issuecomment-808664021
+    // py_run!(py, identity_module, "import sys; sys.modules['libp2p_rust_python_bindings.identity'] = identity_module");
 
     m.add_submodule(identity_module)?;
     Ok(())
